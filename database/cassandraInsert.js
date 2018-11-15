2ec9cc93812273dd0c4e4b966fc46046365a5cc0
Caitlin Detro <caitlindetro@gmail.com>
2018-11-15 14:27:30 -0600

    Correctly creating keyspace and table schemas in cassandra

---
+++ database/cassandraInsert.js
@@ -0,0 +1,60 @@
+const ExpressCassandra = require('express-cassandra');
+
+const models = ExpressCassandra.createClient({
+  clientOptions: {
+      contactPoints: ['127.0.0.1'],
+      protocolOptions: { port: 9042 },
+      keyspace: 'mykeyspace',
+      queryOptions: {consistency: ExpressCassandra.consistencies.one}
+  },
+  ormOptions: {
+    defaultReplicationStrategy: {
+      class: 'SimpleStrategy',
+      replication_factor: 1
+    },
+    migration: 'safe'
+  }
+});
+
+var AgentModel = models.loadSchema('Agent', {
+  fields: {
+    id: 'int',
+    name: 'text',
+    imageUrl: 'text',
+    rating: 'int',
+    phone: 'text',
+    sales: 'int'
+  },
+  key: ['id']
+});
+
+
+AgentModel.syncDB(function(err, result) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('Changed agent schema: ', result);
+  }
+});
+
+var SaleModel = models.loadSchema('Sale', {
+  fields: {
+    id: 'int',
+    price: 'int',
+    sellDate: 'int',
+    beds: 'int',
+    baths: 'int',
+    squareFeet: 'int',
+    address: 'text'
+  },
+  key: ['id']
+});
+
+
+SaleModel.syncDB(function(err, result) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('Changed sale schema: ', result);
+  }
+});
